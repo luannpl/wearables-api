820e42f10d03f02eb045081b2a9ff827a78241d8
Paulo Luan <pauloluan1409@gmail.com>
2025-06-26 00:01:48 -0300

    feat: add endpoint for create product

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -1,27 +1,28 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../../prisma/client";
 
 export const ProductRepository = {
-    async findAll() {
-        try {
-            const products = await prisma.product.findMany({
-                include: {
-                    category: true,
-                },
-            });
-            return products;
-        } catch (error) {
-            throw new Error("Error fetching products");
-        }
-    },
+  async findAll() {
+    try {
+      const products = await prisma.product.findMany({
+        include: {
+          category: true,
+        },
+      });
+      return products;
+    } catch (error) {
+      throw new Error("Error fetching products");
+    }
+  },
 
-    async create(productData: any) {
-        try {
-            const newProduct = await prisma.product.create({
-                data: productData,
-            });
-            return newProduct;
-        } catch (error) {
-            throw new Error("Error creating product");
-        }
-    },
+  async create(data: Prisma.ProductCreateInput) {
+    try {
+      const newProduct = await prisma.product.create({
+        data,
+      });
+      return newProduct;
+    } catch (error) {
+      throw new Error("Error creating product");
+    }
+  },
 };
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,35 +1,80 @@
 import { BadRequestError } from "../../errors/HttpErrors";
+import { prisma } from "../../prisma/client";
 import { CategoryRepository } from "../category/category.repository";
 import { UserRepository } from "../user/user.repository";
 import { CreateProductDto } from "./dto/createProduct.dto";
 import { ProductRepository } from "./product.repository";
 
 export const ProductService = {
-    async getAllProducts() {
-        try {
-            const products = await ProductRepository.findAll();
-            return products;
-        } catch (error) {
-            throw new Error("Error fetching products");
-        }
-    },
-
-    async createProduct(productData: CreateProductDto) {
-        console.log(productData)
-        const category = await CategoryRepository.upsert(productData.category);
-        const user = await UserRepository.findById(productData.registredById);
-        if(!user){
-            throw new BadRequestError("User not exists")
-        }
-        
-        try {
-            const newProduct = await ProductRepository.create(productData);
-            if (!newProduct) {
-                throw new BadRequestError("Product creation failed");
-            }
-            return newProduct;
-        } catch (error) {
-            throw new Error("Error creating product");
-        }
+  async getAllProducts() {
+    try {
+      const products = await ProductRepository.findAll();
+      return products;
+    } catch (error) {
+      throw new Error("Error fetching products");
     }
+  },
+
+  async createProduct(productData: CreateProductDto) {
+    console.log(productData);
+    const category = await CategoryRepository.upsert(productData.category);
+    const user = await UserRepository.findById(productData.registredById);
+    if (!user) {
+      throw new BadRequestError("User not exists");
+    }
+    productData.category = category.id;
+    const { sizes, category: categoryId, registredById, ...rest } = productData;
+    if (!categoryId) {
+      throw new BadRequestError("Category is required");
+    }
+    if (!registredById) {
+      throw new BadRequestError("RegistredById is required");
+    }
+    try {
+      const newProduct = await ProductRepository.create({
+        ...rest,
+        category: {
+          connect: { id: categoryId },
+        },
+        registredBy: {
+          connect: { id: registredById },
+        },
+      });
+      if (!newProduct) {
+        throw new BadRequestError("Product creation failed");
+      }
+      for (const { label, stock } of sizes) {
+        // Garante que o tamanho exista
+        const size = await prisma.size.upsert({
+          where: { label },
+          update: {},
+          create: { label },
+        });
+
+        // Cria a associação ProductSize com o estoque
+        await prisma.productSize.create({
+          data: {
+            productId: newProduct.id,
+            sizeId: size.id,
+            stock,
+          },
+        });
+      }
+      const productWithSizes = await prisma.product.findUnique({
+        where: { id: newProduct.id },
+        include: {
+          category: true,
+          sizes: {
+            include: {
+              size: true,
+            },
+          },
+        },
+      });
+
+      return productWithSizes;
+    } catch (error) {
+      throw new Error("Error creating product");
+    }
+  },
 };
