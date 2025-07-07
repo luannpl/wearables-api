6fde6463f2a2024af96423d94a1301427d0ea50c
Paulo Luan <pauloluan1409@gmail.com>
2025-07-07 10:47:23 -0300

    feat: add required_error

--- src/modules/user/schemas/createUser.schema.ts
+++ src/modules/user/schemas/createUser.schema.ts
@@ -2,12 +2,23 @@ import { z } from 'zod';
 import { Role } from '@prisma/client';
 
 export const CreateUserSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  username: z.string().min(1, "Username is required"),
-  email: z.string().email("Invalid email format").min(1, "Email is required"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  name: z.string({
+    required_error: "Name is required",
+  }).min(1, "Name is required"),
+  username: z.string({
+    required_error: "Username is required",
+  }).min(1, "Username is required"),
+  email: z.string({
+    required_error: "Email is required",
+  }).email("Invalid email format").min(1, "Email is required"),
+  password: z.string({
+    required_error: "Password is required",
+  }).min(6, "Password must be at least 6 characters long"),
   avatarUrl: z.string().optional(),
-  role: z.nativeEnum(Role),
+  role: z.nativeEnum(Role, {
+    required_error: "Role is required",
+    invalid_type_error: "Invalid role value",
+  }),
 });
 
 
